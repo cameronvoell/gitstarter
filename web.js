28c6e3dfc27d85fda2fef6852ba4420180e2355a
Cameron Voell <camsurf@amigo1.cameronvoell.com>
2014-03-15 01:31:37 -0700

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+
+var buf = fs.readFile('index.html')
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldi 2!');
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;

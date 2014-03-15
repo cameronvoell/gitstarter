0bf9a477c6d3b758c459ea8279515695ae3fbab0
Cameron Voell <camsurf@amigo1.cameronvoell.com>
2014-03-15 01:51:10 -0700

    web.js now reads from index.html

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-var buf = fs.readFile('index.html')
-
-
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8"));
+  var buf = fs.readFileSync('index.html')
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
